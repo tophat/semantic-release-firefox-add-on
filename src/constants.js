e69f1d2a7a0da1857cd13b0bc40c3300ad8307f0
Daniel Basilio <daniel.basilio@tophat.com>
2019-08-16 16:06:57 -0400

    move to individual files

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+const defaultOptions = {
+    artifactsDir: './',
+    manifestPath: 'manifest.json',
+    sourceDir: 'dist',
+    targetXpi: 'myextension.xpi',
+}
+
+module.exports = [
+    defaultOptions,
+]
---
+++ src/publish.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const path = require('path')
+const webExt = require('web-ext').default
+
+const defaultOptions = require('./constants')
+
+const publish = async ({ extensionId, ...options }) => {
+    // This will create an unsigned xpi from sourceDir folder (dist) it will
+    // then pass the unsigned xpi to the signing api, mozilla will validate the
+    // xpi and sign it if it's legitimate. They will give us back a signed xpi
+    // which will be placed into artifactsDir
+    // If there's an error with the validation, webExt sign will log a link to
+    // the console which will lead to the validation page which should contain
+    // detailed reasons why the extension was rejected
+    const { artifactsDir, sourceDir, targetXpi } = {
+        ...defaultOptions,
+        ...options,
+    }
+    const { FIREFOX_API_KEY, FIREFOX_SECRET_KEY } = process.env
+    const { success, downloadedFiles } = await webExt.cmd.sign({
+        apiKey: FIREFOX_API_KEY,
+        apiSecret: FIREFOX_SECRET_KEY,
+        artifactsDir,
+        channel: 'unlisted',
+        id: extensionId,
+        sourceDir,
+    })
+    if (!success) {
+        throw new Error(
+            'Signing the extension failed. See the console output from web-ext sign for the validation link',
+        )
+    }
+    const [xpiFile] = downloadedFiles
+    fs.renameSync(
+        path.join(artifactsDir, xpiFile),
+        path.join(artifactsDir, targetXpi),
+    )
+}
+
+module.exports = {
+    publish,
+}

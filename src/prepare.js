3649a22e9c141d9f3a86b1b74ab4d2a88908e174
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-01-11 15:36:21 -0500

    wip: use aggregate error

--- src/prepare.js
+++ src/prepare.js
@@ -13,17 +13,16 @@ const prepare = (options, { nextRelease, logger }) => {
     try {
         manifest = fs.readFileSync(normalizedManifestPath)
     } catch (e) {
-        throw new Error('Unable to read manifest.json file from dist folder', e)
+        throw new Error('Unable to read manifest.json file from dist folder')
     }
 
     try {
-        const jsonManifest = JSON.parse(manifest)
+        const jsonManifest = JSON.parse(manifest.toString())
         jsonManifest.version = version
         manifest = JSON.stringify(jsonManifest, null, 2)
     } catch (e) {
         throw new Error(
             'Failed to parse manifest.json from dist folder into JSON',
-            e,
         )
     }
 
@@ -32,7 +31,6 @@ const prepare = (options, { nextRelease, logger }) => {
     } catch (e) {
         throw new Error(
             'Failed to write updated manifest.json to the dist folder',
-            e,
         )
     }
 

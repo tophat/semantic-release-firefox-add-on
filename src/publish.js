cd50b1b7baafc003ca7fd06c728035b3561ed5f5
iorate <iorate@users.noreply.github.com>
2020-03-14 23:15:43 +0900

    fix: exit successfully when submitting a listed add-on
    Save an unsigned file as targetXpi.

--- src/publish.js
+++ src/publish.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 const webExt = require('web-ext').default
+const defaultAddonSigner = require('sign-addon')
 
 const { verifyOptions } = require('./utils')
 
@@ -22,14 +23,29 @@ const publish = async options => {
     } = verifyOptions(options, ['extensionId', 'targetXpi'])
 
     const { FIREFOX_API_KEY, FIREFOX_SECRET_KEY } = process.env
-    const { success, downloadedFiles } = await webExt.cmd.sign({
-        apiKey: FIREFOX_API_KEY,
-        apiSecret: FIREFOX_SECRET_KEY,
-        artifactsDir,
-        channel,
-        id: extensionId,
-        sourceDir,
-    })
+
+    let unsignedXpiPath
+    const signAddon = async params => {
+        unsignedXpiPath = params.xpiPath
+        const result = await defaultAddonSigner(params)
+        if (!result.success && result.errorCode === 'ADDON_NOT_AUTO_SIGNED') {
+            result.success = true
+            result.downloadedFiles = result.downloadedFiles || []
+        }
+        return result
+    }
+
+    const { success, downloadedFiles } = await webExt.cmd.sign(
+        {
+            apiKey: FIREFOX_API_KEY,
+            apiSecret: FIREFOX_SECRET_KEY,
+            artifactsDir,
+            channel,
+            id: extensionId,
+            sourceDir,
+        },
+        { signAddon },
+    )
     if (!success) {
         throw new Error(
             'Signing the extension failed. See the console output from web-ext sign for the validation link',
@@ -37,7 +53,7 @@ const publish = async options => {
     }
     const [xpiFile] = downloadedFiles
     fs.renameSync(
-        path.join(artifactsDir, xpiFile),
+        xpiFile ? path.join(artifactsDir, xpiFile) : unsignedXpiPath,
         path.join(artifactsDir, targetXpi),
     )
 }
